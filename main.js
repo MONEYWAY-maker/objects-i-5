d346b538c1b9e625825863de7ef8c750a405d15b
MONEYWAY-maker <lenwoodsbiz@gmail.com>
2026-09-24 02:40:27 -0700

    Updated main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Write your code below
+function getAverageOfElements(obj, key) {
+  const value = obj[key];
+
+  if (!Array.isArray(value) || value.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < value.length; i++) {
+    sum += value[i];
+  }
+
+  return sum / value.length;
+}
